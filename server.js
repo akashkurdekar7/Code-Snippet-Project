fcbbc8f02ee8e65acd502cef48e1874ed6dca78f
Akash Kurdekar <akashkurdekar39@gmail.com>
2024-03-20 22:43:46 +0530

    server

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ import userRoute from "./routes/userRoute.js";
 const app = express();
 app.use(cors());
 app.use(express.json());
-
 app.use("/api/users", userRoute);
 
 app.use((err, req, res, next) => {
