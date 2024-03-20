5487116eed511feecbdc3a9ba73340703fe00f9e
Akash Kurdekar <akashkurdekar39@gmail.com>
2024-03-20 20:11:43 +0530

    name, error changes

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,16 +1,33 @@
 import React from "react";
 import Button from "@mui/material/Button";
-import ArrowForwardIcon from "@mui/icons-material/ArrowForward";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
+import { Container, Typography } from "@mui/material";
 
 const Error = () => {
   return (
-    <div>
-      <h1>error</h1>
-
-      <Button variant="contained" startIcon={<ArrowForwardIcon />}>
-        Home
+    <Container
+      maxWidth="md"
+      className="mt-5 d-flex justify-content-center align-items-center flex-column"
+    >
+      <Typography variant="h3" align="center" color="black" gutterBottom>
+        Oops! Something went wrong.
+      </Typography>
+      <Typography variant="h5" align="center" gutterBottom>
+        We're sorry, but it seems there was an error.
+      </Typography>
+      <Typography variant="body1" align="center" gutterBottom>
+        Please try again later or contact support if the problem persists.
+      </Typography>
+      <Button
+        variant="contained"
+        color="success"
+        className="text-center"
+        startIcon={<ArrowBackIcon />}
+        onClick={() => window.history.back()}
+      >
+        Go Back
       </Button>
-    </div>
+    </Container>
   );
 };
 
